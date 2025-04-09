ff971342414ef17af3eb9218ff043c6d56ebeccd
Arshia <87672623+ashiboyman@users.noreply.github.com>
2025-04-09 23:06:55 +0300

    auth added and user table added

--- lib/task_manager3_web/router.ex
+++ lib/task_manager3_web/router.ex
@@ -1,6 +1,8 @@
 defmodule TaskManager3Web.Router do
   use TaskManager3Web, :router
 
+  import TaskManager3Web.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule TaskManager3Web.Router do
     plug :put_root_layout, html: {TaskManager3Web.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
     plug :spy
 
   end
@@ -60,6 +63,44 @@ defmodule TaskManager3Web.Router do
 
       live_dashboard "/dashboard", metrics: TaskManager3Web.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", TaskManager3Web do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{TaskManager3Web.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", TaskManager3Web do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{TaskManager3Web.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", TaskManager3Web do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{TaskManager3Web.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
